19850ae7130c09b92d22c706927f263c399dfaee
mangat-ram <se.mangat.ram@gmail.com>
2026-04-25 21:33:06 +0530

    Added default port.

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import app from "./services/express";
 import { port } from "./config";
 import { connection } from "./services/mongodb";
 
+const portValue = port || 4000; // Default to 3000 if port is not defined
+
 async function startServer() {
   try {
     console.log("Connecting to database...");
@@ -9,8 +11,8 @@ async function startServer() {
 
     console.log("Starting server...");
 
-    app.listen(port, () => {
-      console.log(`Server running on http://localhost:${port}`);
+    app.listen(portValue, () => {
+      console.log(`Server running on http://localhost:${portValue}`);
     });
   } catch (error) {
     console.error("Failed to start server:", error);
